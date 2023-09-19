ec423c72fa122da421c87f2e0b974aafd06b6eca
Philipe Henrique de Sousa Lima <philipehenri.lima@gmail.com>
2023-09-19 17:44:46 -0300

    senha servlet

--- src/main/java/servletWaves/ServletWaves.java
+++ src/main/java/servletWaves/ServletWaves.java
@@ -1,6 +1,8 @@
 package servletWaves;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -36,12 +38,22 @@ public class ServletWaves extends HttpServlet {
 		// TODO Auto-generated method stub
 		
 		response.setContentType("text/html");
+		 
+		//Coleta os dados inseridos no formulário
 		String nome = request.getParameter("nome");
 		String email = request.getParameter("email");
 		String senha = request.getParameter("senha");
 		
-		response.sendRedirect("index.jsp");
-		//teste teste
+		 if (senha != null && senha.length() >= 8 && nome != null && email != null) {
+			 response.sendRedirect("index.jsp");
+		 }else {
+	    	PrintWriter out = response.getWriter();
+			out.print("Existe algo errado! \n");
+			out.print("A senha deve ter 8 ou mais caracteres");
+	     }
+		
+		
+		
 		doGet(request, response);
 	}
 
